94cf747a39a8e088b7663b297fa679ed501b4957
Ashok-IT-School <ashokitschool@gmail.com>
2020-08-21 11:56:03 +0530

    CSL-345 completed

--- DateUtils.java
+++ DateUtils.java
@@ -3,13 +3,7 @@ public class DateUtils{
      int i = 10; 
      int k = 30;
 	 
-	 public void init(){ 
-	 
-	 
-		//logic
-	 
-	 
-	 }
+     public void convert(){ }
      
      public void format(){
           
--- Demo.java
+++ Demo.java
@@ -1,14 +1,11 @@
 public class Demo{
 
+	public void m2(){
+
+	}
+
 	 //Bug-193 fixed
 	public void m1(){
 		
 	}
-
-	
-	//Bug-190 changes
-    public void doProcess(){
-
-
-   }
 }
